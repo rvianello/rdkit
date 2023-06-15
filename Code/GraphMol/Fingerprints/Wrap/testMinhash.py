cb83f24c074a5bf8e0d338adf9e64915547f2719
Riccardo Vianello <riccardo.vianello@gmail.com>
2023-06-15 23:20:10 +0200

    implement basic unit testing of the Minhash python wrapper

--- Code/GraphMol/Fingerprints/Wrap/testMinhash.py
+++ Code/GraphMol/Fingerprints/Wrap/testMinhash.py
@@ -1,8 +1,53 @@
 import unittest
 
+from rdkit import Chem
+from rdkit.Chem import rdFingerprintGenerator
+from rdkit.Chem import rdMinhash
+
+
 class TestCase(unittest.TestCase):
-  pass
+
+  def testMinhashBasic(self):
+    mol1 = Chem.MolFromSmiles('O=C(O)CC1CCC1')
+    mol2 = Chem.MolFromSmiles('O=C(O)CC1CC1')
+
+    fpgen = rdFingerprintGenerator.GetMorganGenerator()
+    bfp1, bfp2 = (fpgen.GetSparseFingerprint(mol) for mol in (mol1, mol2))
+
+    # Hash1
+    siggen = rdMinhash.MinhashSignatureGenerator32H1(256)
+    sig1, sig2 = (siggen(bfp) for bfp in (bfp1, bfp2))
+    similarity = rdMinhash.tanimotoSimilarity(sig1, sig2)
+    self.assertAlmostEqual(similarity, 0.7578125)
+
+    siggen = rdMinhash.MinhashSignatureGenerator16H1(256)
+    sig1, sig2 = (siggen(bfp) for bfp in (bfp1, bfp2))
+    similarity = rdMinhash.tanimotoSimilarity(sig1, sig2)
+    self.assertAlmostEqual(similarity, 0.7578125)
+
+    siggen = rdMinhash.MinhashSignatureGenerator8H1(256)
+    sig1, sig2 = (siggen(bfp) for bfp in (bfp1, bfp2))
+    similarity = rdMinhash.tanimotoSimilarity(sig1, sig2)
+    self.assertAlmostEqual(similarity, 0.765625)
+
+    # Hash2
+    siggen = rdMinhash.MinhashSignatureGenerator32H2(256)
+    sig1, sig2 = (siggen(bfp) for bfp in (bfp1, bfp2))
+    similarity = rdMinhash.tanimotoSimilarity(sig1, sig2)
+    self.assertAlmostEqual(similarity, 0.73046875)
+
+    siggen = rdMinhash.MinhashSignatureGenerator16H2(256)
+    sig1, sig2 = (siggen(bfp) for bfp in (bfp1, bfp2))
+    similarity = rdMinhash.tanimotoSimilarity(sig1, sig2)
+    self.assertAlmostEqual(similarity, 0.73046875)
+
+    siggen = rdMinhash.MinhashSignatureGenerator8H2(256)
+    sig1, sig2 = (siggen(bfp) for bfp in (bfp1, bfp2))
+    similarity = rdMinhash.tanimotoSimilarity(sig1, sig2)
+    self.assertAlmostEqual(similarity, 0.73828125)
+
 
 if __name__ == '__main__':
+
   unittest.main()
 
